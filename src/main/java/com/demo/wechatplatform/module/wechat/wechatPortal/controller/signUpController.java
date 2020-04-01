623eafb99e9b393e93a4fcfdd6ffd0170037f74e
chao920413 <675708945@qq.com>
2020-04-01 16:20:53 +0800

    0.0.1

--- src/main/java/com/demo/wechatplatform/module/wechat/wechatPortal/controller/signUpController.java
+++ src/main/java/com/demo/wechatplatform/module/wechat/wechatPortal/controller/signUpController.java
@@ -1,9 +1,12 @@
 package com.demo.wechatplatform.module.wechat.wechatPortal.controller;
 
+import com.demo.wechatplatform.module.wechat.wechatPortal.dto.access_token;
 import me.chanjar.weixin.mp.bean.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.WxMpXmlOutMessage;
 import me.chanjar.weixin.mp.bean.WxMpXmlOutTextMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping(value = "/signUp")
@@ -38,6 +41,15 @@ public class signUpController {
         }
         out = outMessage.toXml();
         return out;
+    }
+
+    @RequestMapping(value = "getWebToken_AcessByCode")
+    public String getUserInfo(@RequestBody access_token access_token){
+        RestTemplate restTemplate=new RestTemplate();
+        String url="https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code";
+        url=String.format(url,"wx73da6c3a780f0ea2","92809e6b89e09737d59f9a69663c0a34",access_token.getCode());
+        access_token=restTemplate.getForObject(url,access_token.getClass());
+        return null;
     }
 
 }
---
+++ src/main/java/com/demo/wechatplatform/module/wechat/wechatPortal/dto/access_token.java
@@ -0,0 +1,11 @@
+package com.demo.wechatplatform.module.wechat.wechatPortal.dto;
+
+import lombok.Data;
+
+@Data
+public class access_token {
+    /**
+     * 前端获取的code
+     */
+    public String code;
+}
